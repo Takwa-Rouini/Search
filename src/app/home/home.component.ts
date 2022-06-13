e2c5f686f9a16573c3b798d3395b9c1df1f44d1c
takwa <takwa.rouini@ensi-uma.tn>
2022-06-14 00:23:20 +0200

    first push

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+
+import { Flight } from '../Model/flight';
+import { FlightService } from '../service/flight.service';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+ origin !: String  ;
+ destination ! : String ;
+ departuredate! : String;
+flight ! :Flight[];
+
+
+
+
+  constructor(private flihgtService: FlightService) { }
 
   ngOnInit(): void {
   }
+
+getplaces( ){
+   this.flihgtService.getplaces(this.origin , this.destination , this.departuredate).subscribe(data => {
+    this.flight=data;  })
+
+
+}
+switchorigin(origin : String){
+  this.origin = origin ;
+}
+switchdestination( destination : String){
+  this. destination = destination ;
+}
+switchdate( date : String ) {
+  this.departuredate = date ;
+}
+
+
 
 }
